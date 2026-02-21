17046212232529afc2222ea30def7823620ca985
Guilherme Esteves <guiresteves@gmail.com>
2026-02-21 11:39:45 -0300

    feat: adiciona atributos no model de Categoria

--- src/main/java/com/br/stockpro/model/Category.java
+++ src/main/java/com/br/stockpro/model/Category.java
@@ -5,7 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
-@Table(name = "categories")
+@Table(
+        name = "categories",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_category_company_name",
+                        columnNames = {"company_id", "name"}
+                )
+        }
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -20,4 +28,14 @@ public class Category extends Auditable {
     @NotBlank
     @Column(nullable = false)
     private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+    @Column(nullable = false)
+    private Boolean active = true;
 }
