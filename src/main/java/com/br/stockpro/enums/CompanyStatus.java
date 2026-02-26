b18f3fd6cc5bef8955bf08a2a90db4ec46cacf06
Guilherme Esteves <guiresteves@gmail.com>
2026-02-26 14:20:06 -0300

    refactor: alteração no enum de Status da Empresa

--- src/main/java/com/br/stockpro/enums/CompanyStatus.java
+++ src/main/java/com/br/stockpro/enums/CompanyStatus.java
@@ -1,19 +1,9 @@
 package com.br.stockpro.enums;
 
 public enum CompanyStatus {
-    ACTIVE("Pode usar o sistema normalmente"),
-    INACTIVE("Desativada manualmente"),
-    SUSPENDED("Inadimplência"),
-    TRIAL("Período de teste"),
-    CANCELLED("Cancelado");
-
-    private final String description;
-
-    CompanyStatus(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED,
+    TRIAL,
+    CANCELLED;
 }
