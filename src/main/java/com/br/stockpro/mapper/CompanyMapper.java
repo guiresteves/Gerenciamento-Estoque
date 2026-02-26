643d0d638237d38fcacfd1288e80a95dba9ec84e
Guilherme Esteves <guiresteves@gmail.com>
2026-02-26 15:54:55 -0300

    feat: adiciono o Mapper de Empresa no modulo

--- src/main/java/com/br/stockpro/mapper/CompanyMapper.java
+++ src/main/java/com/br/stockpro/mapper/CompanyMapper.java
@@ -1,4 +1,23 @@
 package com.br.stockpro.mapper;
 
+import com.br.stockpro.dtos.company.CompanyCreateRequest;
+import com.br.stockpro.dtos.company.CompanyResponse;
+import com.br.stockpro.dtos.company.CompanyUpdateRequest;
+import com.br.stockpro.model.Company;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CompanyMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    Company toEntity(CompanyCreateRequest dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDTO(
+            CompanyUpdateRequest dto,
+            @MappingTarget Company entity
+    );
+
+    CompanyResponse toResponseDTO(Company company);
 }
