df98fc5b3d7c08a45b7e395e7f7f32dbaea48b8e
Guilherme Esteves <guiresteves@gmail.com>
2026-03-02 15:52:03 -0300

    feat: adiciona a configuração de criptografia

--- src/main/java/com/br/stockpro/repository/UserRepository.java
+++ src/main/java/com/br/stockpro/repository/UserRepository.java
@@ -1,2 +1,12 @@
-package com.br.stockpro.repository;public interface UserRepository {
+package com.br.stockpro.repository;
+
+import com.br.stockpro.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
