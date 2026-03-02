153b1da031c8110518cbfa9a358a65a4bd63b8f5
Guilherme Esteves <guiresteves@gmail.com>
2026-03-02 16:23:17 -0300

    feat: adiciona o service de JWT

---
+++ src/main/java/com/br/stockpro/service/JwtService.java
@@ -0,0 +1,32 @@
+package com.br.stockpro.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "minha-chave-super-secreta-para-desenvolvimento";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = SECRET_KEY.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+
+    }
+}
