08ca061cc28d372db81d117cf88918b06d77fc9c
Guilherme Esteves <guiresteves@gmail.com>
2026-03-21 00:32:03 -0300

    feat: implementa o modulo de autentucação

--- src/main/java/com/br/stockpro/controller/AuthController.java
+++ src/main/java/com/br/stockpro/controller/AuthController.java
@@ -1,2 +1,29 @@
-package com.br.stockpro.controller;public class AuthController {
+package com.br.stockpro.controller;
+
+import com.br.stockpro.dtos.auth.AuthResponse;
+import com.br.stockpro.dtos.auth.LoginRequest;
+import com.br.stockpro.dtos.auth.RegisterRequest;
+import com.br.stockpro.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    public final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.register(registerRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
 }
