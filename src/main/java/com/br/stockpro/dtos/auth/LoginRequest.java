8f91b5ad274b157b42f1dd31c183210149c195fd
Guilherme Esteves <guiresteves@gmail.com>
2026-03-25 23:24:20 -0300

    feat: adiciona validação no dtos

--- src/main/java/com/br/stockpro/dtos/auth/LoginRequest.java
+++ src/main/java/com/br/stockpro/dtos/auth/LoginRequest.java
@@ -1,8 +1,15 @@
 package com.br.stockpro.dtos.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
 
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email inválido")
         String email,
+
+        @NotBlank(message = "Senha é obrigatória")
         String password
 ) {
 }
