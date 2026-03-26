297937814e0b2d9b755e04994e393b61583eb83b
Guilherme Esteves <guiresteves@gmail.com>
2026-03-26 16:15:55 -0300

    feat: alteração od dtos de update de Empresa

--- src/main/java/com/br/stockpro/dtos/company/CompanyUpdateRequest.java
+++ src/main/java/com/br/stockpro/dtos/company/CompanyUpdateRequest.java
@@ -5,11 +5,9 @@ import jakarta.validation.constraints.*;
 
 public record CompanyUpdateRequest(
 
-        @NotBlank(message = "Razão social é obrigatória")
         @Size(min = 3, max = 250, message = "Razão social deve ter entre 3 e 250 caracteres")
         String legalName,
 
-        @NotBlank(message = "Nome fantasia é obrigatório")
         @Size(min = 3, max = 250, message = "Nome fantasia deve ter entre 3 e 250 caracteres")
         String tradeName,
 
@@ -19,15 +17,12 @@ public record CompanyUpdateRequest(
         @Size(max = 50, message = "Inscrição municipal deve ter no máximo 50 caracteres")
         String municipalRegistration,
 
-        @NotNull(message = "Tipo da empresa é obrigatório")
         CompanyType companyType,
 
-        @NotBlank(message = "Telefone é obrigatório")
         @Size(max = 20, message = "Telefone deve ter no máximo 20 caracteres")
         @Pattern(regexp = "[0-9()\\-\\s+]+", message = "Telefone inválido")
         String phone,
 
-        @NotBlank(message = "Email é obrigatório")
         @Email(message = "Email inválido")
         @Size(max = 150, message = "Email deve ter no máximo 150 caracteres")
         String email
