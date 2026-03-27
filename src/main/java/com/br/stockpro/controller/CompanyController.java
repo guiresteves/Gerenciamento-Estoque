e36e6966c5906e8df682d5fdb057783af20b844a
Guilherme Esteves <guiresteves@gmail.com>
2026-03-27 01:21:23 -0300

    feat altera o Controller de Empresa

--- src/main/java/com/br/stockpro/controller/CompanyController.java
+++ src/main/java/com/br/stockpro/controller/CompanyController.java
@@ -9,21 +9,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/companies")
+@RequestMapping("/api/companies")
 @RequiredArgsConstructor
 public class CompanyController {
 
     private final CompanyService companyService;
 
-    @GetMapping
-    public ResponseEntity<CompanyResponse> getCompany() {
-        return ResponseEntity.ok(companyService.getCompany());
+    @GetMapping("/me")
+    public ResponseEntity<CompanyResponse> getMyCompany() {
+        return ResponseEntity.ok(companyService.getMyCompany());
     }
 
-    @PutMapping
-    public ResponseEntity<CompanyResponse> update(
-            @RequestBody @Valid CompanyUpdateRequest request) {
-
-        return ResponseEntity.ok(companyService.update(request));
+    @PutMapping("/me")
+    public ResponseEntity<CompanyResponse> updateMyCompany(
+            @RequestBody @Valid CompanyUpdateRequest request
+    ) {
+        return ResponseEntity.ok(companyService.updateMyCompany(request));
     }
 }
