2435525b28bf1b87d5d79f8d177b6e0c19f805ec
Guilherme Esteves <guiresteves@gmail.com>
2026-03-28 09:55:31 -0300

    feat: adiciona a função de criar um Empresa com o user Autenticado

--- src/main/java/com/br/stockpro/service/CompanyService.java
+++ src/main/java/com/br/stockpro/service/CompanyService.java
@@ -1,5 +1,6 @@
 package com.br.stockpro.service;
 
+import com.br.stockpro.dtos.company.CompanyCreateRequest;
 import com.br.stockpro.dtos.company.CompanyResponse;
 import com.br.stockpro.dtos.company.CompanyUpdateRequest;
 import com.br.stockpro.exceptions.BusinessException;
@@ -8,6 +9,7 @@ import com.br.stockpro.mapper.CompanyMapper;
 import com.br.stockpro.model.Company;
 import com.br.stockpro.model.User;
 import com.br.stockpro.repository.CompanyRepository;
+import com.br.stockpro.repository.UserRepository;
 import com.br.stockpro.security.AuthenticatedUserService;
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +24,29 @@ public class CompanyService {
     private final CompanyRepository companyRepository;
     private final CompanyMapper companyMapper;
     private final AuthenticatedUserService authenticatedUserService;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public CompanyResponse createMyCompany(CompanyCreateRequest request) {
+
+        User currentUser = authenticatedUserService.getCurrentUser();
+
+        if (currentUser.getCompany() != null) {
+            throw new NotFoundException("Usuário já possui empresa vinculada");
+        }
+
+        if (companyRepository.existsByCnpj(request.cnpj())) {
+            throw new NotFoundException("Usuário não possui empresa vinculada");
+        }
+
+        Company company = companyMapper.toEntity(request);
+        Company savedCompany = companyRepository.save(company);
+
+        currentUser.setCompany(savedCompany);
+        userRepository.save(currentUser);
+
+        return companyMapper.toResponse(savedCompany);
+    }
 
     @Transactional(readOnly = true)
     public CompanyResponse getMyCompany() {
@@ -55,7 +80,6 @@ public class CompanyService {
         }
 
         companyMapper.updateEntityFromDTO(request, company);
-
         Company updatedCompany = companyRepository.save(company);
 
         return companyMapper.toResponse(updatedCompany);
