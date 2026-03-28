55b6f3532064e8e0282f2772381180c5fd1c7814
Guilherme Esteves <guiresteves@gmail.com>
2026-03-28 14:21:28 -0300

    feat: adiciona metodos no Repository de Categoria

--- src/main/java/com/br/stockpro/repository/CategoryRepository.java
+++ src/main/java/com/br/stockpro/repository/CategoryRepository.java
@@ -3,9 +3,16 @@ package com.br.stockpro.repository;
 import com.br.stockpro.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByIdAndCompanyId(Long id, Long companyId);
+
+    List<Category> findAllByByCompanyId(Long companyId);
+
+    boolean existsByNameIgnoreCaseAndCompanyId(String name, Long companyId);
+
+    boolean existsByNameIgnoreCaseAndCompanyIdAndIdNot(String name, Long companyId, Long id);
 }
