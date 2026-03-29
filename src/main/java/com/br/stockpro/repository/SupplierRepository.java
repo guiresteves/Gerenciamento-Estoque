7df90a8d8b479626bba5d8fb2bae726aa0721aef
Guilherme Esteves <guiresteves@gmail.com>
2026-03-29 15:27:35 -0300

    feat: adiciona o Repository no modulo de Fornecedor

---
+++ src/main/java/com/br/stockpro/repository/SupplierRepository.java
@@ -0,0 +1,18 @@
+package com.br.stockpro.repository;
+
+import com.br.stockpro.model.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+
+    Optional<Supplier> findByIdAndCompanyId(Long id, Long companyId);
+
+    List<Supplier> findAllByCompanyId(Long companyId);
+
+    boolean existsByCnpjByCompanyId(String cnpj, Long companyId);
+
+    boolean existsByCnpjAndCompanyIdAndIdNot(String cnpj, Long companyId, Long id);
+}
