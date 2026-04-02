5e833c552e4cb59203a95d7eb475190583522965
Guilherme Esteves <guiresteves@gmail.com>
2026-04-02 02:36:18 -0300

    feat: altera todo o service do modulo de Produto

--- src/main/java/com/br/stockpro/service/ProductService.java
+++ src/main/java/com/br/stockpro/service/ProductService.java
@@ -4,110 +4,145 @@ import com.br.stockpro.dtos.product.ProductCreateRequest;
 import com.br.stockpro.dtos.product.ProductResponse;
 import com.br.stockpro.dtos.product.ProductUpdateRequest;
 import com.br.stockpro.exceptions.BusinessException;
+import com.br.stockpro.exceptions.NotFoundException;
 import com.br.stockpro.mapper.ProductMapper;
 import com.br.stockpro.model.Category;
 import com.br.stockpro.model.Company;
 import com.br.stockpro.model.Product;
+import com.br.stockpro.model.User;
 import com.br.stockpro.repository.CategoryRepository;
-import com.br.stockpro.repository.CompanyRepository;
 import com.br.stockpro.repository.ProductRepository;
+import com.br.stockpro.security.AuthenticatedUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
-    private final CompanyRepository companyRepository;
     private final CategoryRepository categoryRepository;
     private final ProductMapper productMapper;
+    private final AuthenticatedUserService authenticatedUserService;
 
     @Transactional
-    public ProductResponse create(ProductCreateRequest request) {
+    public ProductResponse createProduct(ProductCreateRequest request){
 
-        Company company = companyRepository.findById(request.companyId())
-                .orElseThrow(() -> new BusinessException("Empresa não encontrada"));
+        User currentUser = authenticatedUserService.getCurrentUser();
+        Company company = getCurrentUserCompany(currentUser);
 
-        Category category = categoryRepository.findById(request.categoryId())
-                .orElseThrow(() -> new BusinessException("Categoria não encontrada"));
-
-        if (!category.getCompany().getId().equals(company.getId())) {
-            throw new BusinessException("Categoria não pertence à empresa informada");
+        if (productRepository.existsByBarcodeAndCompanyId(request.barcode(), company.getId())){
+            throw new BusinessException("Já existe um produto com esse código de barra");
         }
 
-        boolean barcodeExists = productRepository
-                .existsByCompanyIdAndBarcode(company.getId(), request.barcode());
-
-        if (barcodeExists) {
-            throw new BusinessException("Já existe produto com esse código de barras nesta empresa");
-        }
+        Category category = categoryRepository.findByIdAndCompanyId(request.categoryId(), company.getId())
+                .orElseThrow(() -> new NotFoundException("Categoria não encontrada"));
 
         Product product = productMapper.toEntity(request);
-
-        product.setCompany(company);
         product.setCategory(category);
+        product.setCompany(company);
 
-        Product saved = productRepository.save(product);
-
-        return productMapper.toResponse(saved);
-    }
-
-    @Transactional
-    public ProductResponse update(Long id, ProductUpdateRequest request) {
-
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new BusinessException("Produto não encontrado"));
-
-        productMapper.updateEntityFromDTO(request, product);
-
-        if (request.categoryId() != null) {
-
-            Category category = categoryRepository.findById(request.categoryId())
-                    .orElseThrow(() -> new BusinessException("Categoria não encontrada"));
-
-            if (!category.getCompany().getId().equals(product.getCompany().getId())) {
-                throw new BusinessException("Categoria não pertence à empresa do produto");
-            }
-
-            product.setCategory(category);
-        }
-
-        Product updated = productRepository.save(product);
-
-        return productMapper.toResponse(updated);
-    }
-
-    @Transactional(readOnly = true)
-    public ProductResponse findById(Long id) {
-
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new BusinessException("Produto não encontrado"));
-
+        productRepository.save(product);
         return productMapper.toResponse(product);
     }
 
     @Transactional(readOnly = true)
-    public List<ProductResponse> findAll() {
+    public List<ProductResponse> findAllProducts(){
 
-        return productRepository.findAllByActiveTrue()
+        User currentUser = authenticatedUserService.getCurrentUser();
+        Company company = getCurrentUserCompany(currentUser);
+
+        return productRepository.findAllByCompanyId(company.getId())
                 .stream()
                 .map(productMapper::toResponse)
                 .toList();
     }
 
-    @Transactional
-    public void delete(Long productId, Long companyId) {
+    @Transactional(readOnly = true)
+    public ProductResponse findProductById(Long id){
 
-        Product product = productRepository
-                .findByIdAndCompanyIdAndActiveTrue(productId, companyId)
-                .orElseThrow(() -> new BusinessException("Produto não encontrado"));
+        User currentUser = authenticatedUserService.getCurrentUser();
+        Company company = getCurrentUserCompany(currentUser);
+
+        Product product = productRepository.findByIdAndCompanyId(id, company.getId())
+                .orElseThrow(() -> new NotFoundException("Produto não encontrado"));
+
+        return productMapper.toResponse(product);
+    }
+
+    public ProductResponse updateProduct(Long id, ProductUpdateRequest request){
+
+        User currentUser = authenticatedUserService.getCurrentUser();
+        Company company = getCurrentUserCompany(currentUser);
+
+        Product product = productRepository.findByIdAndCompanyId(id, company.getId())
+                .orElseThrow(() -> new NotFoundException("Produto não encontrado"));
+
+        if (!Boolean.TRUE.equals(product.getActive())){
+            throw new BusinessException("Não é possivel alterar produto inativo");
+        }
+
+        if (request.barcode() != null &&
+                productRepository.existsByBarcodeAndCompanyIdAndIdNot(
+                        request.barcode(),
+                        company.getId(),
+                        product.getId()
+        )) {
+            throw new BusinessException("Já existe um produto com esse código de barras para esta empresa");
+        }
+
+        if (request.categoryId() != null){
+
+            Category category = categoryRepository.findByIdAndCompanyId(request.categoryId(), company.getId())
+                    .orElseThrow(() -> new NotFoundException("Categoria não encontrada"));
+
+            product.setCategory(category);
+        }
+
+        productMapper.updateEntityFromDTO(request, product);
+
+        Product updatedProduct = productRepository.save(product);
+        return productMapper.toResponse(updatedProduct);
+    }
+
+    @Transactional
+    public ProductResponse activate(Long id){
+
+        User currentUser = authenticatedUserService.getCurrentUser();
+        Company company = getCurrentUserCompany(currentUser);
+
+        Product product = productRepository.findByIdAndCompanyId(id, company.getId())
+                .orElseThrow(() -> new NotFoundException("Produto não encontrado"));
+
+        product.setActive(true);
+
+        Product updatedProduct = productRepository.save(product);
+        return productMapper.toResponse(updatedProduct);
+    }
+
+    @Transactional
+    public ProductResponse deactivate(Long id){
+
+        User currentUser = authenticatedUserService.getCurrentUser();
+        Company company = getCurrentUserCompany(currentUser);
+
+        Product product = productRepository.findByIdAndCompanyId(id, company.getId())
+                .orElseThrow(() -> new NotFoundException("Produto não encontrado"));
 
         product.setActive(false);
 
-        productRepository.save(product);
+        Product updatedProduct = productRepository.save(product);
+        return productMapper.toResponse(updatedProduct);
+    }
+
+    private Company getCurrentUserCompany(User currentUser) {
+        if (currentUser.getCompany() == null) {
+            throw new BusinessException("Usuário não possui empresa vinculada");
+        }
+        return currentUser.getCompany();
     }
 }
