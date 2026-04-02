33eefc60c7eee7ca90d5e712a1a683df99161868
Guilherme Esteves <guiresteves@gmail.com>
2026-04-02 00:56:34 -0300

    feat: alterar o mapper do modulo der Produto

--- src/main/java/com/br/stockpro/mapper/ProductMapper.java
+++ src/main/java/com/br/stockpro/mapper/ProductMapper.java
@@ -2,9 +2,8 @@ package com.br.stockpro.mapper;
 
 import com.br.stockpro.dtos.product.ProductCreateRequest;
 import com.br.stockpro.dtos.product.ProductResponse;
-import com.br.stockpro.dtos.product.ProductUpdatedRequest;
+import com.br.stockpro.dtos.product.ProductUpdateRequest;
 import com.br.stockpro.model.Category;
-import com.br.stockpro.model.Company;
 import com.br.stockpro.model.Product;
 import org.mapstruct.*;
 
@@ -12,28 +11,22 @@ import org.mapstruct.*;
 public interface ProductMapper {
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "company", source = "companyId")
+    @Mapping(target = "company", ignore = true)
     @Mapping(target = "category", source = "categoryId")
     @Mapping(target = "active", constant = "true")
     Product toEntity(ProductCreateRequest dto);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "company", ignore = true)
     @Mapping(target = "category", source = "categoryId")
-    void updateEntityFromDTO(ProductUpdatedRequest dto, @MappingTarget Product entity);
+    void updateEntityFromDTO(ProductUpdateRequest dto, @MappingTarget Product entity);
 
     @Mapping(target = "companyId", source = "company.id")
     @Mapping(target = "companyLegalName", source = "company.legalName")
     @Mapping(target = "categoryId", source = "category.id")
     @Mapping(target = "categoryName", source = "category.name")
     ProductResponse toResponse(Product product);
-
-    default Company mapCompany(Long companyId) {
-        if (companyId == null) return null;
-        return Company.builder()
-                .id(companyId)
-                .build();
-    }
 
     default Category mapCategory(Long categoryId) {
         if (categoryId == null) return null;
