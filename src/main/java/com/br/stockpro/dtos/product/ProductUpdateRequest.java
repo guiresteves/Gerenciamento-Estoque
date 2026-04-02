cb66c89b5883db8aecf6a01e800a97d14a57b592
Guilherme Esteves <guiresteves@gmail.com>
2026-04-02 00:39:30 -0300

    feat: padronização dos DTOs do modulo de Produto

--- src/main/java/com/br/stockpro/dtos/product/ProductUpdateRequest.java
+++ src/main/java/com/br/stockpro/dtos/product/ProductUpdateRequest.java
@@ -5,35 +5,29 @@ import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 
-public record ProductCreateRequest(
+public record ProductUpdateRequest(
 
-        @NotBlank(message = "Nome é obrigatório")
         @Size(min = 3, max = 250, message = "Nome deve ter entre 3 e 250 caracteres")
         String name,
 
         @Size(max = 500, message = "A descrição deve ter no máximo 500 caracteres")
         String description,
 
-        @NotBlank(message = "Código de barras é obrigatório")
         @Size(min = 8, max = 50, message = "O código de barras deve ter entre 8 e 50 caracteres")
         String barcode,
 
-        @NotNull(message = "A unidade de medida é obrigatória")
         UnitOfMeasure unitOfMeasure,
 
         Boolean trackExpiration,
 
-        @NotNull(message = "O preço de custo é obrigatório")
         @DecimalMin(value = "0.00", message = "O preço de custo deve ser maior ou igual a zero")
         @Digits(integer = 10, fraction = 2)
         BigDecimal costPrice,
 
-        @NotNull(message = "O preço de venda é obrigatório")
         @DecimalMin(value = "0.00", message = "O preço de venda deve ser maior ou igual a zero")
         @Digits(integer = 10, fraction = 2)
         BigDecimal salePrice,
 
-        @NotNull(message = "Categoria é obrigatória")
         Long categoryId
 ) {
 }
