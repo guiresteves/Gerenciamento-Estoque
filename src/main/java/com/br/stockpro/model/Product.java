a3df93d9f54af4cdf0f0900b008cd3d6aac28065
Guilherme Esteves <guiresteves@gmail.com>
2026-04-01 23:52:44 -0300

    refactor: padronização no model de Produtos

--- src/main/java/com/br/stockpro/model/Product.java
+++ src/main/java/com/br/stockpro/model/Product.java
@@ -2,10 +2,6 @@ package com.br.stockpro.model;
 
 import com.br.stockpro.enums.UnitOfMeasure;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -15,7 +11,6 @@ import java.math.BigDecimal;
 @Setter
 @Table(
         name = "products",
-
         uniqueConstraints = {
                 @UniqueConstraint(
                         columnNames = {"company_id", "barcode"}
@@ -35,37 +30,30 @@ public class Product extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
-    @Column(nullable = false)
+    @Column(nullable = false, length = 250)
     private String name;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(length = 500)
     private String description;
 
-    @NotBlank
-    @Column(name = "barcode", nullable = false)
+    @Column(nullable = false, length = 50)
     private String barcode;
 
-    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private UnitOfMeasure unitOfMeasure;
 
+    @Builder.Default
     @Column(name = "track_expiration", nullable = false)
-    private Boolean trackExpiration;
+    private Boolean trackExpiration = false;
 
+    @Builder.Default
     @Column(nullable = false)
-    private Boolean active;
+    private Boolean active = true;
 
-    @NotNull
-    @DecimalMin(value = "0.00", inclusive = true)
-    @Digits(integer = 10, fraction = 2)
     @Column(name = "cost_price", nullable = false)
     private BigDecimal costPrice;
 
-    @NotNull
-    @DecimalMin(value = "0.00", inclusive = true)
-    @Digits(integer = 10, fraction = 2)
     @Column(name = "sale_price", nullable = false)
     private BigDecimal salePrice;
 
