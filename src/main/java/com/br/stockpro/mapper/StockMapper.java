17afcdd308ba38d7db2ecef265c41b089a0fb98b
Guilherme Esteves <guiresteves@gmail.com>
2026-04-08 22:19:45 -0300

    feat: implementa o Mapper do modulo de Stock

---
+++ src/main/java/com/br/stockpro/mapper/StockMapper.java
@@ -0,0 +1,42 @@
+package com.br.stockpro.mapper;
+
+import com.br.stockpro.dtos.stock.StockCreateRequest;
+import com.br.stockpro.dtos.stock.StockResponse;
+import com.br.stockpro.dtos.stock.StockUpdateRequest;
+import com.br.stockpro.model.Category;
+import com.br.stockpro.model.Company;
+import com.br.stockpro.model.Product;
+import com.br.stockpro.model.Stock;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface StockMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "company", ignore = true)
+    @Mapping(target = "product", source = "productId")
+    @Mapping(target = "active", constant = "true")
+    Stock toEntity(StockCreateRequest dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "company", ignore = true)
+    @Mapping(target = "product", ignore = true)
+    void updateEntityFromDTO(StockUpdateRequest dto, @MappingTarget Stock entity);
+
+    @Mapping(target = "productId", source = "product.id")
+    @Mapping(target = "productName", source = "product.name")
+    @Mapping(target = "productBarcode", source = "product.barcode")
+    @Mapping(target = "companyId", source = "company.id")
+    @Mapping(target = "availableQuantity", expression = "java(stock.getAvailableQuantity())")
+    @Mapping(target = "belowMinimum", expression = "java(stock.isBelowMinimum())")
+    StockResponse toResponse(Stock stock);
+
+    default Product mapProduct(Long productId) {
+        if (productId == null) return null;
+
+        return Product.builder()
+                .id(productId)
+                .build();
+    }
+}
