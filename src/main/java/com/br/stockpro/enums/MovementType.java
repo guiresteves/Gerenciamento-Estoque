06da1bb370ba853eb0ab5da277a8f0ee4de64370
Guilherme Esteves <guiresteves@gmail.com>
2026-04-15 01:09:05 -0300

    feat: adiciona o enum de tipos de movimentos no estoque

---
+++ src/main/java/com/br/stockpro/enums/MovementType.java
@@ -0,0 +1,14 @@
+package com.br.stockpro.enums;
+
+public enum MovementType {
+
+    ENTRADA,
+    SAIDA,
+    AJUSTE_NEGATIVO,
+    AJUSTE_POSITIVO,
+    RESERVA,
+    LIBERACAO,
+    VENCIMENTO,
+    DEVOLUCAO
+
+}
