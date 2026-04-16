0a129fbc04ac29b7014e54a4074267dcac3c1c45
Guilherme Esteves <guiresteves@gmail.com>
2026-04-16 00:36:44 -0300

    refactor: adiciona as validações no DTO de StockMovementManulRequest

--- src/main/java/com/br/stockpro/dtos/stockMovement/StockMovementManualRequest.java
+++ src/main/java/com/br/stockpro/dtos/stockMovement/StockMovementManualRequest.java
@@ -1,12 +1,21 @@
 package com.br.stockpro.dtos.stockMovement;
 
 import com.br.stockpro.enums.MovementType;
+import jakarta.validation.constraints.*;
 
 public record StockMovementManualRequest(
 
+        @NotNull(message = "O ID do produto é obrigatório")
         Long productId,
+
+        @NotNull(message = "O tipo de movimento é obrigatório")
         MovementType movementType,
+
+        @NotNull(message = "A quantidade é obrigatória")
+        @Min(value = 1, message = "A quantidade deve ser maior que zero")
         Integer quantity,
+
+        @Size(max = 255, message = "O motivo deve ter no máximo 255 caracteres")
         String reason
 
 ) {
