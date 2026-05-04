670bd8bdc5c83018991d5eb10b1acb96908b1731
Guilherme Esteves <guiresteves@gmail.com>
2026-05-03 22:02:33 -0300

    docs: liberação do swagger na config do Security

--- src/main/java/com/br/stockpro/config/SecurityConfig.java
+++ src/main/java/com/br/stockpro/config/SecurityConfig.java
@@ -26,6 +26,14 @@ public class SecurityConfig {
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs"
+                        ).permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
